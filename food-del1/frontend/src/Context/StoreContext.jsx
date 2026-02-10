97f8fe48597151cf0e1027c4004cffcfa36f3828
bitescampus27-maker <bitescampus27@gmail.com>
2026-02-10 23:04:37 +0530

    Update StoreContext.jsx

--- food-del1/frontend/src/Context/StoreContext.jsx
+++ food-del1/frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:5000";
+    const url = "https://campus-bite-backend.onrender.com";
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
